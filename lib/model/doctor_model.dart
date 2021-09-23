c15451b7d07368d9a8ae1ab0b166088e5020aed0
kuvw <kazimlumia@gmail.com>
2021-09-23 19:04:30 +0300

    adding doctor model and doctor dumb

--- lib/model/doctor_model.dart
+++ lib/model/doctor_model.dart
@@ -1,4 +1,43 @@
 class DoctorModel {
-  String name;
-  String attendence_date;
+  final Map<String, String> _available_dates;
+  final String name;
+
+  DoctorModel(this._available_dates, this.name);
+}
+
+class DumpData {
+  final DoctorModel drMustafa =
+      DoctorModel({'Tusday': '6:00 AM - 12:00 PM'}, 'Dr.Mustafa ghani');
+  final DoctorModel drHamza = DoctorModel({
+    'Sunday': '2:00 PM - 8:30 PM',
+    'Monday': '2:00 PM - 8:30 PM',
+    'Tusday': '2:00 PM - 8:30 PM',
+    'Winsday': '2:00 PM - 8:30 PM',
+    'Friday': '8:30 AM - 8:30 PM',
+    'Saturday': '8:30 AM - 8:30 PM',
+  }, 'Dr.Hamza ali');
+  final DoctorModel drMouda = DoctorModel({
+    'Sunday': '2:00 PM - 8:30 PM',
+    'Monday': '2:00 PM - 8:30 PM',
+    'Tusday': '2:00 PM - 8:30 PM',
+    'Winsday': '2:00 PM - 8:30 PM',
+    'Friday': '8:30 AM - 8:30 PM',
+    'Saturday': '8:30 AM - 8:30 PM',
+  }, 'Dr.Mouda adeeb');
+  final DoctorModel drMalath = DoctorModel({
+    'Sunday': '2:00 PM - 8:30 PM',
+    'Monday': '2:00 PM - 8:30 PM',
+    'Tusday': '2:00 PM - 8:30 PM',
+    'Winsday': 'off',
+    'Friday': 'off',
+    'Saturday': '8:30 AM - 8:30 PM',
+  }, 'Dr.Malath');
+  final DoctorModel drAyman = DoctorModel({
+    'Sunday': '2:00 PM - 8:30 PM',
+    'Monday': '2:00 PM - 8:30 PM',
+    'Tusday': '2:00 PM - 8:30 PM',
+    'Winsday': 'off',
+    'Friday': 'off',
+    'Saturday': '8:30 AM - 8:30 PM',
+  }, 'Dr.Ayman salah al-deen');
 }
